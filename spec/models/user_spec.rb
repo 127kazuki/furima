d41189bd29b674602d18802ff83cbc4dfcd9683c
127kazuki <77536433+127kazuki@users.noreply.github.com>
2021-04-18 14:08:53 +0900

    expectの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,36 +9,42 @@ require 'rails_helper'
      it "nicknameが空だと登録できない" do
       @user.nickname = ''
       @user.valid?
-   expect(user.errors.full_messages).to include("Nickname can't be blank")
+   expect(@user.errors.full_messages).to include("Nickname can't be blank")
  end
      it "emailが空では登録できない" do
       @user.email = ''
       @user.valid?
-   expect(user.errors.full_messages).to include("Email can't be blank")
+   expect(@user.errors.full_messages).to include("Email can't be blank")
  end
  it "passwordが空だと登録できない" do
   @user.password = ''
       @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be blank")
 end
  it "last_nameが空だと登録できない" do
       @user.last_name = ''
       @user.valid?
+      expect(@user.errors.full_messages).to include("Last name can't be blank")
  end
  it "first_nameが空だと登録できない" do
   @user.first_name = ''
       @user.valid?
+      expect(@user.errors.full_messages).to include("First name can't be blank")
  end
  it "last_name_kanaが空だと登録できない" do
   @user.last_name_kana = ''
       @user.valid?
+      expect(@user.errors.full_messages).to include("Last name kana can't be blank")
  end
  it "first_name_kanaが空だと登録できない" do
   @user.first_name_kana = ''
       @user.valid?
+      expect(@user.errors.full_messages).to include("First name kana can't be blank")
  end
  it "birthdayが空だと登録できない" do
   @user.birthday = ''
       @user.valid?
+      expect(@user.errors.full_messages).to include("Birthday can't be blank")
  end
    end
  end
