c8af394ecd281eac940a403be8b9aa2d972694f8
127kazuki <77536433+127kazuki@users.noreply.github.com>
2021-04-18 14:31:10 +0900

    パスワード英数混合のバリデーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
          validates :nickname, presence: true
          validates :email, presence: true, uniqueness: true, inclusion: { in: ["@"] }
-         validates :password, confirmation: true, length: { minimum: 6 }
+         validates :password, confirmation: true, length: { minimum: 6 }, format: { with: PASSWORD_REGEX }
          validates :last_name, presence: true
          validates :first_name, presence: true
          validates :last_name_kana, presence: true
