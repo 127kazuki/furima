c40f028ace0ffbb2d0806d9401a294b6671a370a
127kazuki <77536433+127kazuki@users.noreply.github.com>
2021-04-24 16:00:03 +0900

    画像関連以外投稿できるようになった

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,10 +6,18 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
-  private
+  def create
+    @item = Item.create(item_params)
+    if @item.save
+       redirect_to root_path
+    else
+      render :new
+    end
+  end
 
+  private
   def item_params
-    params.require(:item).permit(:title, :explain, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:title,:explain,:category_id,:status_id,:delivery_pay_id,:delivery_day_id,:delivery_area_id,:price).merge(user_id: current_user.id)
   end
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -4,18 +4,18 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
-  belongs_to :delivery_area
+  belongs_to :delivery_pay
   belongs_to :delivery_day
   belongs_to :delivery_area
 
   with_options presence: true do
     validates :title
     validates :explain
-    validates :category
-    validates :status
-    validates :delivery_area
-    validates :delivery_day
-    validates :delivery_pay
+    validates :category_id
+    validates :status_id
+    validates :delivery_area_id
+    validates :delivery_day_id
+    validates :delivery_pay_id
     validates :price, numericality: { in: 300..9999999 }, format: { with: /\A[0-9]+\z/ }
   end
 
