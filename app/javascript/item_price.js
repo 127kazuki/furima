81fdc9cceca55e020c0ce33a61fe94e93a58e2cc
127kazuki <77536433+127kazuki@users.noreply.github.com>
2021-04-29 15:25:54 +0900

    利益表示

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -5,6 +5,7 @@ window.addEventListener('load', () => {
     console.log(inputValue);
     const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
-
+    const profitDom = document.getElementById("profit");
+    profitDom.innerHTML = Math.floor(inputValue - addTaxDom.innerHTML);
    })
 })
