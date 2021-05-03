3309f2daef65f7ada31ada349072016dd9c37017
127kazuki <77536433+127kazuki@users.noreply.github.com>
2021-05-03 11:41:20 +0900

    削除

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
 
   before_action :authenticate_user!, except: [ :index, :show]
-  before_action :iteminfo, only: [:show, :edit, :update, :destroy, :baria_user]
+  before_action :iteminfo, only: [:show, :edit, :update, :destroy]
   before_action :baria_user, only: [:edit, :update, :destroy]
   
 
