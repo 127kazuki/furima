be76a0d1fd9c6c505e48d7730d6a4aba0c2ff2f8
127kazuki <77536433+127kazuki@users.noreply.github.com>
2021-05-08 12:29:48 +0900

    テーブル整え

--- db/migrate/20210508031737_create_buyings.rb
+++ db/migrate/20210508031737_create_buyings.rb
@@ -2,6 +2,8 @@ class CreateBuyings < ActiveRecord::Migration[6.0]
   def change
     create_table :buyings do |t|
 
+      t.references :user, nul: false, foreign_key: true
+      t.references :item, nul: false, foreign_key: true
       t.timestamps
     end
   end
