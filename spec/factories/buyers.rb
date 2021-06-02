76fb7bee820129c4b242723f6fc50608bebc6ce2
127kazuki <77536433+127kazuki@users.noreply.github.com>
2021-06-02 19:19:09 +0900

    factorybotまで

--- spec/factories/buyers.rb
+++ spec/factories/buyers.rb
@@ -1,5 +1,11 @@
 FactoryBot.define do
   factory :buyer do
-    
+      zip_code              {'111-1111'}
+      delivery_area_id     {2}
+      city              {'中央区'}
+      street             {'銀座'}
+      building             {''}
+      phone_number            {'090-0000-0000'}
+      buying_id       {1}
   end
 end
--- spec/models/buyer_spec.rb
+++ spec/models/buyer_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Buyer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @buyer = FactoryBot.build(:buyer)
+  end
+
+  describe '商品購入機能' do
+    context '商品購入ができる時' do
+
+      it "全ての値が正しい" do
+        expect(@buyer).to be_valid
+      end
+
+    end
+    
 end
